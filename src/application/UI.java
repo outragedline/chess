a8e9d623f140839407018a187d903a545de36ab5
outragedline <diegojanson31@outlook.com>
2022-09-29 01:41:14 -0300

    checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,12 +56,18 @@ public class UI {
 		System.out.println();
 		printCapturedPieces(captured);
 		System.out.println();
-		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting for player: " + chessMatch.getCurrentPlayer());
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Turn: " + chessMatch.getTurn());
+			System.out.println("Waiting for player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
 
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+			}
+		}
 
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
